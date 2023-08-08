b8170a81118c1571583aa96e1b2c38f02216f048
etemkocaaslan <kaslanetem@gmail.com>
2023-08-07 20:15:32 -0700

    Refactored ImageDownloader for CLI interaction
    - Enhanced ImageDownloader class to use default parameters for ease of instantiation.
    - Introduced argparse to the main execution, enabling users to specify search queries, number of images, and output folder through the command line.
    - Removed redundant hardcoded values in the main execution, leveraging the default parameters set in the class.

--- bing_photo_fetcher.py
+++ bing_photo_fetcher.py
@@ -1,21 +1,24 @@
+import argparse
 from bing_image_downloader import downloader
 
 class ImageDownloader:
-    def __init__(self, search_queries, num_images_per_query, output_folder_name):
+    def __init__(self, search_queries=["Football", "soccer"], num_images_per_query= 100, output_folder_name="images"):
         self.search_queries = search_queries
         self.num_images_per_query = num_images_per_query
         self.output_folder_name = output_folder_name
 
     def download_images(self):
-    	for query in self.search_queries:
-        	downloader.download(query, limit=self.num_images_per_query, output_dir=self.output_folder_name, force_replace=False, timeout=60)
-
+        for query in self.search_queries:
+            downloader.download(query, limit=self.num_images_per_query, output_dir=self.output_folder_name, force_replace=False, timeout=60)
 
 if __name__ == "__main__":
-    search_queries = ["Football, soccer"]
-    num_images_per_query = 100
-    output_folder_name = 'images'
+    parser = argparse.ArgumentParser(description="Download images using Bing Image Downloader.")
+    parser.add_argument("--search_queries", nargs="+", default=["Football", "soccer"], help="List of search queries to download images for.")
+    parser.add_argument("--num_images_per_query", type=int, default=100, help="Number of images to download for each query.")
+    parser.add_argument("--output_folder_name", default="images", help="Folder to save downloaded images.")
 
-    img_downloader = ImageDownloader(search_queries, num_images_per_query, output_folder_name)
+    args = parser.parse_args()
+
+    img_downloader = ImageDownloader(args.search_queries, args.num_images_per_query, args.output_folder_name)
     img_downloader.download_images()
 
