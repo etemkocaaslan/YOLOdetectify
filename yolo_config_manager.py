139a9b3134f75f5bd488a0c4cfdf70a59503d2ce
etemkocaaslan <kaslanetem@gmail.com>
2023-08-08 01:30:45 -0700

    Update yolo_config_manager.py
    - Updated default paths to reflect the Custom Football Dataset directory.
    - Modified save_to_yaml() to directly write data in desired format.
    - Removed redundant load_from_yaml function.

--- yolo_config_manager.py
+++ yolo_config_manager.py
@@ -2,7 +2,7 @@ import argparse
 import yaml
 
 class YOLOConfig:
-    def __init__(self, train_path='/dataset/train', val_path='/dataset/val', nc=2, names=["Ball Carrier", "Player"]):
+    def __init__(self, train_path='/home/etem/Documents/GitHub/YOLOdetectify/Custom Football Dataset/train', val_path='/home/etem/Documents/GitHub/YOLOdetectify/Custom Football Dataset/val', nc=2, names=["Ball Carrier", "Player"]):
         self.config = {
             'train': train_path,
             'val': val_path,
@@ -12,17 +12,21 @@ class YOLOConfig:
     
     def save_to_yaml(self, filename='data.yaml'):
         with open(filename, 'w') as file:
-            yaml.dump(self.config, file, default_flow_style=False)
-    
-    def load_from_yaml(self, filename='model.yaml'):
-        with open(filename, 'r') as file:
-            self.config = yaml.load(file, Loader=yaml.FullLoader)
-        return self.config
+            # Writing to the file directly to maintain the custom format
+            file.write(f"train: {self.config['train']}\n")
+            file.write(f"val: {self.config['val']}\n\n")
+            file.write(f"nc: {self.config['nc']}\n\n")
+            file.write("names: [")
+            for i, name in enumerate(self.config['names']):
+                if i != 0:
+                    file.write(", ")
+                file.write(f'"{name}"')
+            file.write("]\n")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create YOLO Config File with Custom Parameters')
-    parser.add_argument('--train_path', default='/dataset/train', help='Path to the training data.')
-    parser.add_argument('--val_path', default='/dataset/val', help='Path to the validation data.')
+    parser.add_argument('--train_path', default='/home/etem/Documents/GitHub/YOLOdetectify/Custom Football Dataset/train', help='Path to the training data.')
+    parser.add_argument('--val_path', default='/home/etem/Documents/GitHub/YOLOdetectify/Custom Football Dataset/val', help='Path to the validation data.')
     parser.add_argument('--nc', type=int, default=2, help='Number of classes.')
     parser.add_argument('--names', nargs='+', default=["Ball Carrier", "Player"], help='Names of the classes.')
 
